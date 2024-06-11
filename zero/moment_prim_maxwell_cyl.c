57e6abb4dff2501a25a4698423042ddd987f55f1
Kiran Eiden <keiden@eidensystems.com>
2024-06-11 18:02:07 -0400

    Revert Maxwell cylindrical flux back to original formula
    - The additional factor of r is included in the field components

--- zero/moment_prim_maxwell_cyl.c
+++ zero/moment_prim_maxwell_cyl.c
@@ -21,14 +21,13 @@ gkyl_maxwell_cyl_flux(double c, const double q[9], double flux[9])
 {
   double c2 = c*c;
   double r = fabs(q[6]*q[7]*q[8]);
-  double r2 = r*r;
   double g_ii = q[6]*q[6];
 
   flux[ER] = 0.0;
-  flux[EP] = c2*q[BZ];
-  flux[EZ] = -c2*r2*q[BP]/g_ii;
+  flux[EP] = c2/r*q[BZ];
+  flux[EZ] = -c2*r*q[BP]/g_ii;
   flux[BR] = 0.0;
-  flux[BP] = -q[EZ];
-  flux[BZ] = q[EP]*r2/g_ii;
+  flux[BP] = -q[EZ]/r;
+  flux[BZ] = q[EP]*r/g_ii;
   flux[6] = flux[7] = flux[8] = 0.0;
 }
